6e8203353b300d6fd7627509963136277966ea9e
DToxVanilla <dtoxvanilla@gmail.com>
2022-02-10 22:41:46 +0000

    working on a front end functionality, not UI yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+import { Buttonm, IconButton, TextField } from '@material-ui/core';
+import { Assignment, Phone } from '@material-ui/icons';
+import { useEffect, useRef, useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Peer from 'simple-peer';
+import { io } from 'socket.io-client';
+//css that I may turn to module.css later:
 import './App.css';
 
+const socket = io.connect('http://localhost:3500'); //running this backend on 3500;
+
+
+
 function App() {
+	const [ me, setMe ] = useState("");
+	const [ stream, setStream ] = useState();
+	const [ receivingCall, setReceivingCall ] = useState(false);
+	const [ caller, setCaller ] = useState("");
+	const [ callerSignal, setCallerSignal ] = useState();
+	const [ callAccepted, setCallAccepted ] = useState(false);
+	const [ idToCall, setIdToCall ] = useState("");
+	const [ callEnded, setCallEnded] = useState(false);
+	const [ name, setName ] = useState("");
+  //getting all necessary refs:
+	const myVideo = useRef();
+	const userVideo = useRef();
+  //disconnecting when ending call set:
+	const connectionRef = useRef();
+
+  useEffect(() => {
+
+    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(stream => {
+      setStream(stream);
+      myVideo.current.srcObject = stream;
+    });
+
+    socket.on('me', (id) => {
+      setMe(id);
+    });
+
+    socket.on('callUser', (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
+
+  }, []);
+    //set up for calling a user:
+    const callUser = (id) => {
+      const peer = new Peer({
+        initiator: true,
+        trickle: false,
+        stream: stream
+      });
+
+      peer.on('signal', (data) => {
+        socket.emit('callUser', {
+          userToCall: id,
+          signalData: data,
+          from: me,
+          name: name
+        });
+      });
+
+      peer.on('stream', (stream) => {
+        userVideo.current.srcObject = stream;
+      });
+
+      socket.on('callAccepted', (signal) => {
+        setCallAccepted(true);
+        peer.signal(signal);
+      });
+      
+      connectionRef.current = peer;
+
+    };
+
+    //set up for answering a call:
+    const answerCall = () => {
+
+      setCallAccepted(true);
+      const peer = new Peer({
+        initiator: false,
+        trickle: false,
+        stream: stream
+      });
+
+      
+
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
